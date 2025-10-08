bb2a6a4b5398242daf2adf64f4e94497cf13524f
Anton <anton.croner@gmail.com>
2025-10-08 18:35:34 +0200

    Global declaration file.

---
+++ global.d.ts
@@ -0,0 +1,10 @@
+declare global {
+	namespace WebAssembly {
+		class Suspending extends Function {
+			readonly name = "Suspending"
+			constructor(asyncFunction: (...args: any[]) => any)
+		}
+		function promising(exportFunction: (...args: any[]) => any): (...args: any[]) => Promise<any>
+	}
+}
+export {}
--- jspi.d.ts
+++
@@ -1,7 +0,0 @@
-declare namespace WebAssembly {
-	class Suspending extends Function {
-		readonly name = "Suspending"
-		constructor(asyncFunction: (...args: any[]) => any)
-	}
-	function promising(exportFunction: (...args: any[]) => any): (...args: any[]) => Promise<any>
-}
